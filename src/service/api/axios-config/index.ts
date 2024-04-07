874b05c18d4dcc2502cc2a9d036908e2c1d60c8e
Milena Leme <milenaleme4@hotmail.com>
2024-04-07 09:33:32 -0300

    inserir alteração do console.log

--- src/service/api/axios-config/index.ts
+++ src/service/api/axios-config/index.ts
@@ -2,8 +2,6 @@ import axios, { AxiosInstance } from 'axios';
 import { errorInterceptor, responseInterceptor } from './interceptors';
 import { Environment } from '@/env';
 
-console.log(Environment.URL_BASE);
-
 const api: AxiosInstance = axios.create({
   baseURL: Environment.URL_BASE,
   headers: {
