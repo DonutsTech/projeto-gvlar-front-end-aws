2c703f71aad54ed80294cd644b49e3aa70a4b5d8
Milena Leme <milenaleme4@hotmail.com>
2024-04-06 23:12:40 -0300

    teste sem back-end

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,5 +1,7 @@
 export const Environment = {
-  URL_BASE: 'https://projeto-gvlar-back-end.onrender.com',
+  URL_BASE: '',
   URL_IMAGE: 'https://image-gvlar.s3.amazonaws.com/',
-  URL_DOMINIO: 'https://projeto-gvlar-front-end.onrender.com',
+  URL_DOMINIO: '.',
 };
+
+//https://projeto-gvlar-back-end.onrender.com back-end;
