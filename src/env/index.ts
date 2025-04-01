3e32bfc6184fb2607916777644b289b9c9aefd0a
Milena Leme <milenaleme4@hotmail.com>
2025-03-31 22:43:44 -0300

    novo env

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,5 +1,5 @@
 export const Environment = {
-  URL_BASE: 'https://ec2-54-224-169-231.compute-1.amazonaws.com:3000',
+  URL_BASE: 'http://ec2-54-224-169-231.compute-1.amazonaws.com:3000',
   URL_IMAGE: 'https://gvlar.s3.us-east-1.amazonaws.com/',
   URL_DOMINIO: 'https://gvlar.com.br',
 };
